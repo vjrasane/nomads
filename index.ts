40aa5725c6d2b2cc20b58bac1e96b7f56848b175
Ville Räsänen <ville.rasanen@appgyver.com>
2022-05-20 15:27:04 +0300

    move index file to project root

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { Maybe, Just, Nothing } from './src/monads/maybe';
+export { Either, Left, Right } from './src/monads/either';
+export { Result, Ok, Err } from './src/monads/result';
+export { RemoteData, StandBy, Loading, Success, Failure } from './src/monads/remote-data';
+export { Task } from './src/monads/task/internal';
+export { Tuple } from './src/monads/tuple';
